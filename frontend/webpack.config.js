81f5b86d3bace163f2024c1c36957e1e57689fac
Roni <roniyildirim888@gmail.com>
2025-06-02 19:34:06 +0300

    change on  vercel.json config file

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,65 +2,93 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: isProduction ? 'static/js/[name].[contenthash].js' : 'bundle.js',
+      publicPath: '/',
+      clean: true // Clean the output directory before emit
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader', 'postcss-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: isProduction ? 'static/media/[name].[hash][ext]' : '[name][ext]'
+          }
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: isProduction ? 'static/fonts/[name].[hash][ext]' : '[name][ext]'
           }
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html'
-    }),
-    new CopyPlugin({
-      patterns: [
-        { 
-          from: "public", 
-          to: ".",
-          globOptions: {
-            ignore: ["**/index.html"],
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html',
+        minify: isProduction ? {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        } : false
+      }),
+      new CopyPlugin({
+        patterns: [
+          { 
+            from: "public", 
+            to: ".",
+            globOptions: {
+              ignore: ["**/index.html"],
+            },
           },
-        },
-      ],
-    }),
-  ],
-  devServer: {
-    historyApiFallback: true,
-    port: 3000,
-    hot: true,
-    static: {
-      directory: path.join(__dirname, 'public'),
-    }
-  }
+        ],
+      }),
+    ],
+    devServer: {
+      historyApiFallback: true,
+      port: 3000,
+      hot: true,
+      static: {
+        directory: path.join(__dirname, 'public'),
+      }
+    },
+    optimization: isProduction ? {
+      splitChunks: {
+        chunks: 'all',
+      },
+    } : {},
+  };
 }; 
